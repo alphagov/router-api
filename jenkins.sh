b0f161254b7034416c4cc88d99b599d27fc1d1c5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-14 15:10:42 +0000

    Add jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+export RAILS_ENV=test
+
+git clean -fdx
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+
+bundle exec rake db:drop
+bundle exec rake ci:setup:rspec default
