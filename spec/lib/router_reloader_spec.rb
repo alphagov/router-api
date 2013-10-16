39b1d51b98f83177d662e40319df87b22c830c94
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-16 15:38:29 +0000

    Avoid hardcoding router reload port.
    Change config file to store a list of URLs instead.

--- spec/lib/router_reloader_spec.rb
+++ spec/lib/router_reloader_spec.rb
@@ -5,12 +5,12 @@ describe RouterReloader do
 
   describe "triggering a reload of routes" do
     before :each do
-      RouterReloader.stub(:router_hosts).and_return(["foo.example.com", "bar.example.com"])
+      RouterReloader.stub(:router_urls).and_return(["http://foo.example.com:1234/reload", "http://bar.example.com:4321/reload"])
     end
 
     it "should POST to the reload endpoint on all the configured routers" do
-      r1 = stub_request(:post, "http://foo.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 200)
-      r2 = stub_request(:post, "http://bar.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 200)
+      r1 = stub_request(:post, "http://foo.example.com:1234/reload").to_return(:status => 200)
+      r2 = stub_request(:post, "http://bar.example.com:4321/reload").to_return(:status => 200)
 
       RouterReloader.reload
 
@@ -20,21 +20,21 @@ describe RouterReloader do
 
     context "error handling" do
       it "should send an exception notification on HTTP errors" do
-        r1 = stub_request(:post, "http://foo.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 401, :body => "Authorisation required")
-        r2 = stub_request(:post, "http://bar.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 404, :body => "Not found")
+        r1 = stub_request(:post, "http://foo.example.com:1234/reload").to_return(:status => 401, :body => "Authorisation required")
+        r2 = stub_request(:post, "http://bar.example.com:4321/reload").to_return(:status => 404, :body => "Not found")
 
         expect(ExceptionNotifier).to receive(:notify_exception) {|ex, options|
           expect(ex.message).to eq("Failed to trigger reload on some routers")
           errors = options[:data][:errors]
-          expect(errors[0]).to eq({:host => "foo.example.com", :status => "401", :body => "Authorisation required"})
-          expect(errors[1]).to eq({:host => "bar.example.com", :status => "404", :body => "Not found"})
+          expect(errors[0]).to eq({:url => "http://foo.example.com:1234/reload", :status => "401", :body => "Authorisation required"})
+          expect(errors[1]).to eq({:url => "http://bar.example.com:4321/reload", :status => "404", :body => "Not found"})
         }
         RouterReloader.reload
       end
 
       it "should still reload subsequent hosts on error" do
-        r1 = stub_request(:post, "http://foo.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 401, :body => "Authorisation required")
-        r2 = stub_request(:post, "http://bar.example.com:#{RouterReloader::ROUTER_RELOAD_PORT}/reload").to_return(:status => 200)
+        r1 = stub_request(:post, "http://foo.example.com:1234/reload").to_return(:status => 401, :body => "Authorisation required")
+        r2 = stub_request(:post, "http://bar.example.com:4321/reload").to_return(:status => 200)
 
         RouterReloader.reload
 
