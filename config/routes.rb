0ba92edc46b78b1024800491dc7592ca510bb2df
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-18 10:27:06 +0100

    Disable formatted route for healthcheck.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ RouterApi::Application.routes.draw do
 
   resources :backends, :only => [:show, :update]
 
-  get "/healthcheck" => proc { [200, {}, ["OK"]] }
+  get "/healthcheck" => proc { [200, {}, ["OK"]] }, :format => false
 end
