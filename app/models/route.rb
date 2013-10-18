74eaa0223a1aed03fa1c2a504e4b757e8debf441
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-10-18 14:23:47 +0000

    Validate a route's incoming_path
    Must be a full URL path

--- app/models/route.rb
+++ app/models/route.rb
@@ -9,6 +9,7 @@ class Route
   ensure_index [[:incoming_path, 1], [:route_type, 1]], :unique => true
 
   validates :incoming_path, :presence => true, :uniqueness => {:scope => :route_type}
+  validate :validate_incoming_path
   validates :route_type, :inclusion => {:in => %w(prefix exact)}
   validates :handler, :inclusion => {:in => %w(backend)}
   with_options :if => :backend? do |be|
@@ -23,6 +24,19 @@ class Route
   end
 
   private
+
+  def validate_incoming_path
+    return if self.incoming_path.blank? # handled by presence validation
+    errors[:incoming_path] << "is not a valid absolute URL path" unless valid_incoming_path?
+  end
+
+  def valid_incoming_path?
+    return false unless self.incoming_path.starts_with?("/")
+    uri = URI.parse(self.incoming_path)
+    uri.path == self.incoming_path
+  rescue URI::InvalidURIError
+    false
+  end
 
   def validate_backend_id
     return if self.backend_id.blank? # handled by presence validation
