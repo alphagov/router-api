788c978d6281c05ba407d7a58c8bc2b515e1bf63
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-11-06 13:57:27 +0000

    Add sample seed file for seeding external redirects.
    The actual redirects will live in alphagov-deployment

---
+++ db/seeds/external_redirects.rb
@@ -0,0 +1,22 @@
+######################################
+#
+# This file is run on every deploy.
+# Ensure any changes account for this.
+# XXX: This will be overwritten on deploy!
+#
+######################################
+
+external_redirects = [
+  # %w(/foo http://www.example.com/),
+]
+external_redirects.each do |from, to|
+  puts "Adding redirect from #{from} (exact) -> #{to}"
+  route = Route.find_or_initialize_by_incoming_path_and_route_type(from, 'exact')
+  route.handler = 'redirect'
+  route.redirect_to = to
+  route.redirect_type = 'temporary'
+  route.save :validate => false
+end
+
+require 'router_reloader'
+RouterReloader.reload
