f09b894a7f8c3a6cadbd4b933302e7ebb973d91b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-11-14 11:08:39 +0000

    Downgrade to Rails 3.2
    So that we can use Mongoid (it doesn't support Rails 4 yet)

--- Gemfile
+++ Gemfile
@@ -1,16 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.1'
+gem 'rails', '3.2.15'
 
 gem 'mongo_mapper', '0.13.0.beta2'
 gem 'bson_ext', '1.9.2'
 gem 'logstasher', '0.3.0'
-
-
-group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
-end
 
 gem 'unicorn', '4.6.3'
 gem 'exception_notification', '4.0.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ RouterApi::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -18,6 +18,9 @@ RouterApi::Application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
 
 
 end
