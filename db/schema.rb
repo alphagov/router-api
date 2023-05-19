1de69f1ad29e64b3b786098771f19c9a2bab78d7
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-05-19 16:23:53 +0100

    Add migrations for postgres db

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_111147) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "backends", force: :cascade do |t|
+    t.string "backend_id"
+    t.string "backend_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["backend_id"], name: "index_backends_on_backend_id", unique: true
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "incoming_path"
+    t.string "route_type"
+    t.string "handler"
+    t.boolean "disabled", default: false
+    t.string "backend_id"
+    t.string "redirect_to"
+    t.string "redirect_type"
+    t.string "segments_mode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["incoming_path"], name: "index_routes_on_incoming_path", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
+    t.string "organisation_slug"
+    t.string "organisation_content_id"
+    t.string "app_name"
+    t.text "permissions"
+    t.boolean "remotely_signed_out", default: false
+    t.boolean "disabled", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
