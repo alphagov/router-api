0634428f3068c369dcbb9bbf1e40047d0ceccc42
Sean Rankine <sean.rankine@me.com>
2024-08-30 15:59:13 +0100

    Unset the backend_id for redirects and gone routes
    Backends should only be set for routes with the "backend" handler.
    Redirects and gone routes are handled directly in router and are not
    proxied. This creates confusion in the data model, as redirect can
    currently have a backend (however, its not sent to the backend).

--- app/models/route.rb
+++ app/models/route.rb
@@ -36,6 +36,7 @@ class Route
   end
 
   before_validation :default_segments_mode
+  before_save :remove_backend_id_for_non_backend_routes
   after_create :cleanup_child_gone_routes
 
   scope :excluding, ->(route) { where(id: { :$ne => route.id }) }
@@ -130,6 +131,10 @@ private
     unless Backend.where(backend_id:).exists?
       errors.add(:backend_id, "does not exist")
     end
+  end
+
+  def remove_backend_id_for_non_backend_routes
+    self.backend_id = nil unless backend?
   end
 
   def cleanup_child_gone_routes
